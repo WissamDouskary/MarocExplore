f1242751856489bc89a3e7eba7a410d478a528aa
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-11 15:41:27 +0000

    create Destination model

---
+++ app/Models/Destination.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'itinerary_id',
+        'name',
+        'lodging',
+        'places_to_visit',
+    ];
+
+    protected $casts = [
+        'places_to_visit' => 'array',
+    ];
+
+    public function itinerary()
+    {
+        return $this->belongsTo(Itinerary::class);
+    }
+}
