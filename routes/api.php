c1f68d7f86af3be1dfe1fcc2877fc04d44b45af6
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-11 10:19:08 +0000

    create show all itinerary, register, login, logout, store, update, destroy API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\itineraryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// public routes
+
+//user
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//itineraries
+Route::get('/itineraries', [itineraryController::class, 'index']);
+Route::get('/itineraries/{id}', [itineraryController::class, 'show']);
+Route::get('itineraries/search/{name}', [itineraryController::class, 'search']);
+
+
+// protected routes
+Route::group(['middleware' => ['auth:sanctum']] , function(){
+
+    //itineraries
+    Route::post('/itineraries', [itineraryController::class, 'store']);
+    Route::put('/itineraries/{id}', [itineraryController::class, 'update']);
+    Route::delete('/itineraries/{id}', [itineraryController::class, 'delete']);
+
+    //user
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
