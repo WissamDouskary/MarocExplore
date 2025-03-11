a2f693c6b088aafe9ce025b1d4d12f5c08bbe98f
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-11 15:41:55 +0000

    upgrade itinerary model with relations between it and distinations and user models

--- app/Models/itinerary.php
+++ app/Models/itinerary.php
@@ -2,11 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Destination;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class itinerary extends Model
+class Itinerary extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'categorie', 'duration', 'image', 'destinationslist', 'user_id'];
+
+    protected $fillable = [
+        'title',
+        'categorie',
+        'duration',
+        'image',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function destinations()
+    {
+        return $this->hasMany(Destination::class);
+    }
 }
+
